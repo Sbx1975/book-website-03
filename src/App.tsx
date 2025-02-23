849433758355e35aafe564d2b24facb645ab0dce
Giovanni Ruggiero <ruggiero.gia@gmail.com>
2025-02-23 22:27:48 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function App() {
       
       <section className="relative min-h-screen flex items-center justify-center pt-16">
         <div className="absolute inset-0 z-0">
-          <div className="absolute inset-0 bg-gradient-to-b from-blue-500/20 to-purple-500/20 mix-blend-overlay" :key="image"></div>
+          <div className="absolute inset-0 bg-gradient-to-b from-blue-500/20 to-purple-500/20 mix-blend-overlay"></div>
           <img className="book-img"
             src={bookFrontImg}
             alt="Sak Yant"
